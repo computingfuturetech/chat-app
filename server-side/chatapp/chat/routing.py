bdd6d6229590cc43f46bda0e543a307dfee8ff7b
anasircft <anasircft@gmail.com>
2024-04-03 10:57:49 +0500

    add documents and audio functionality

--- server-side/chatapp/chat/routing.py
+++ server-side/chatapp/chat/routing.py
@@ -1,9 +1,9 @@
 # routing.py
 
 from django.urls import re_path
-from .consumers import ChatConsumer,NotificationConsumer
+from .consumers import ChatConsumer
 
 websocket_urlpatterns = [
     re_path(r'ws/chat/(?P<roomId>\d+)/(?P<userId>\d+)/$', ChatConsumer.as_asgi()),
-    re_path(r'ws/notifications/$', NotificationConsumer.as_asgi()),
+    
 ]
